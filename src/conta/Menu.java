cf3db869604cc88855e7e4e8bff37121c92deda6
vitoriamanuela0 <contatodavitoria@gmail.com>
2024-11-08 01:28:29 -0300

    Atualizando

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,13 +1,21 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
+		        // Teste da Classe Conta
+				Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+				c1.visualizar();
+				c1.sacar(12000.0f);
+				c1.visualizar();
+				c1.depositar(5000.0f);
+				c1.visualizar();
+				
 		Scanner leia = new Scanner(System.in);
 		
 		int opcao;
@@ -41,7 +49,7 @@ public class Menu {
 				
 				 System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				 Sobre();
-	leia.close();
+				 leia.close();
 				 System.exit(0);
 			}
 			
