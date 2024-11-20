17f9d715c7c6e5ee0e003c750ff0abe962f0da38
vitoriamanuela0 <contatodavitoria@gmail.com>
2024-11-20 04:40:48 -0300

    Entrando

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -4,22 +4,29 @@ import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import conta.controller.ContaController;
 import contabancaria.model.ContaCorrente;
 import contabancaria.model.ContaPoupanca;
 import contabancaria.util.Cores;
 
 public class Menu {
 
-	public static Scanner leia = new Scanner(System.in);
-
+	
 	public static void main(String[] args) {
-
-		int opcao = 0;
+		
+		Scanner leia= new Scanner (System.in);
+		
+		ContaController contas = new ContaController();
+		
+		
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
 
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "Adriana", 10000.0f, 1000.0f);
 		cc1.visualizar();
-		cc1.sacar(12000.0f);
 		cc1.visualizar();
 		cc1.depositar(5000.0f);
 		cc1.visualizar();
@@ -28,7 +35,6 @@ public class Menu {
 		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Victor", 100000.0f, 15);
 		cp1.visualizar();
 		cp1.sacar(1000.0f);
-		cp1.visualizar();
 		cp1.depositar(5000.0f);
 		cp1.visualizar();
 
@@ -73,11 +79,24 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println("\n Criar Conta");
-
+				
+				System.out.println("Digite o número da Agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o Nome do Titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+				
+				do { 
+					System.out.println("Digite o Tipo da Conta (1-CC ou 2-CP: ");
+				}while(tipo < 1 && tipo > 2);
+					
+				}
+				
 				keyPress();
 				break;
 			case 2:
 				System.out.println("\n Listar todas as Contas");
+				contas.listarTodas();
 
 				keyPress();
 				break;
@@ -118,7 +137,7 @@ public class Menu {
 				break;
 			}
 		}
-	}
+
 
     	public static void sobre() {
 		System.out.println("\n*********************************************************");
